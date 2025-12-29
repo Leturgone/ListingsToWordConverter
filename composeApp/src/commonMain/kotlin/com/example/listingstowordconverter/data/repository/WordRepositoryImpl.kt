ceca216b4a318d4c8f5decde954a99948b064aa0
Leturgone <finekon221@gmail.com>
2025-12-30 00:04:56 +0300

    created WordRepositoryImpl

---
+++ composeApp/src/commonMain/kotlin/com/example/listingstowordconverter/data/repository/WordRepositoryImpl.kt
@@ -0,0 +1,10 @@
+package compose.project.listingsconverter.data.repository
+
+import compose.project.listingsconverter.domain.model.FileWithCodeModel
+import compose.project.listingsconverter.domain.repository.WordRepository
+
+class WordRepositoryImpl(): WordRepository {
+    override suspend fun convertFilesToWord(files: List<FileWithCodeModel>): Result<String> {
+        TODO("Not yet implemented")
+    }
+}
