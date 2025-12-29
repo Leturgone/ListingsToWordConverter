07e10e6452440b12d61b7b9dcd07440e41977cd3
Leturgone <finekon221@gmail.com>
2025-12-29 23:49:01 +0300

    deleted actual file system

--- composeApp/src/commonMain/kotlin/com/example/listingstowordconverter/data/source/FileSystem.kt
+++
@@ -1,8 +0,0 @@
-package compose.project.listingsconverter.data.source
-
-expect object FileSystem {
-    fun exists(path: Path): Boolean
-    fun metadata(path: Path): FileMetadata
-    fun list(path: Path): List<Path>
-    fun read(path: Path): String
-}
