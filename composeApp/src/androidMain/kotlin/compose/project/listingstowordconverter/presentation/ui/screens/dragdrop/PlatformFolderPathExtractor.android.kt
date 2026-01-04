80880ea21e6992baf69c0b82a1b7326465224d24
Leturgone <finekon221@gmail.com>
2026-01-04 19:05:24 +0300

    add actual PlatformFolderPathExtractor

---
+++ composeApp/src/androidMain/kotlin/compose/project/listingstowordconverter/presentation/ui/screens/dragdrop/PlatformFolderPathExtractor.android.kt
@@ -0,0 +1,13 @@
+package compose.project.listingstowordconverter.presentation.ui.screens.dragdrop
+
+import androidx.compose.ui.draganddrop.DragAndDropEvent
+
+actual class PlatformFolderPathExtractor actual constructor() {
+    actual fun extractFolderPath(event: DragAndDropEvent): String? {
+        TODO("Not yet implemented")
+    }
+
+    actual fun supportsFolderDrag(event: DragAndDropEvent): Boolean {
+        TODO("Not yet implemented")
+    }
+}
