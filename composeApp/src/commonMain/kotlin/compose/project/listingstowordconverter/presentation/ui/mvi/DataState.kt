68b95b2313445660dde1b0752b0b2dbaea3bcca4
Leturgone <finekon221@gmail.com>
2026-01-04 15:27:11 +0300

    rename data state

---
+++ composeApp/src/commonMain/kotlin/compose/project/listingstowordconverter/presentation/ui/mvi/DataState.kt
@@ -0,0 +1,7 @@
+package compose.project.listingstowordconverter.presentation.ui.mvi
+
+sealed class DataState {
+    object Loading : DataState()
+    data class Success(val filePath: String) : DataState()
+    data class Error(val message: String) : DataState()
+}
--- composeApp/src/commonMain/kotlin/compose/project/listingstowordconverter/presentation/ui/mvi/DateState.kt
+++
@@ -1,7 +0,0 @@
-package compose.project.listingstowordconverter.presentation.ui.mvi
-
-sealed class DateState {
-    object Loading : DateState()
-    data class Success(val filePath: String) : DateState()
-    data class Error(val message: String) : DateState()
-}
