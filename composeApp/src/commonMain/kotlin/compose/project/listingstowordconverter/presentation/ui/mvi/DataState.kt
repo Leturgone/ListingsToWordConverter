188c115c621453bfd5cfd2aaf905ec4b1f9bd358
Leturgone <finekon221@gmail.com>
2026-01-05 00:59:28 +0300

    add default state

--- composeApp/src/commonMain/kotlin/compose/project/listingstowordconverter/presentation/ui/mvi/DataState.kt
+++ composeApp/src/commonMain/kotlin/compose/project/listingstowordconverter/presentation/ui/mvi/DataState.kt
@@ -4,4 +4,6 @@ sealed class DataState {
     object Loading : DataState()
     data class Success(val filePath: String) : DataState()
     data class Error(val message: String) : DataState()
+
+    object Default : DataState()
 }
