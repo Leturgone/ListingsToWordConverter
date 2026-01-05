77c133de0dc3c38430814e601d783db8687c3921
Leturgone <finekon221@gmail.com>
2026-01-05 13:03:35 +0300

    realise actual PlatformFolderPathExtractor for android

--- composeApp/src/androidMain/kotlin/compose/project/listingstowordconverter/presentation/ui/components/dragdrop/PlatformFolderPathExtractor.android.kt
+++ composeApp/src/androidMain/kotlin/compose/project/listingstowordconverter/presentation/ui/components/dragdrop/PlatformFolderPathExtractor.android.kt
@@ -3,11 +3,7 @@ package compose.project.listingstowordconverter.presentation.ui.components.dragd
 import androidx.compose.ui.draganddrop.DragAndDropEvent
 
 actual class PlatformFolderPathExtractor actual constructor() {
-    actual fun extractFolderPath(event: DragAndDropEvent): String? {
-        TODO("Not yet implemented")
-    }
+    actual fun extractFolderPath(event: DragAndDropEvent): String? = null
 
-    actual fun supportsFolderDrag(event: DragAndDropEvent): Boolean {
-        TODO("Not yet implemented")
-    }
+    actual fun supportsFolderDrag(event: DragAndDropEvent): Boolean = false
 }
