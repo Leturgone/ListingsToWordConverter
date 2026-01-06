b71f62b078fce0866129eecce8b2d550c455d4bc
Leturgone <finekon221@gmail.com>
2026-01-06 16:27:10 +0300

    add DesktopApp

---
+++ composeApp/src/jvmMain/kotlin/compose/project/listingstowordconverter/DesktopApp.kt
@@ -0,0 +1,13 @@
+package compose.project.listingstowordconverter
+
+import androidx.compose.material.MaterialTheme
+import androidx.compose.runtime.Composable
+import compose.project.listingstowordconverter.presentation.ui.screens.DesktopUploadScreen
+
+
+@Composable
+fun DesktopApp() {
+    MaterialTheme {
+        DesktopUploadScreen()
+    }
+}
