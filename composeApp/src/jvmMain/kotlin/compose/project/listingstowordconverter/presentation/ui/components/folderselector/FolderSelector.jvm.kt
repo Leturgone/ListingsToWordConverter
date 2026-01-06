f0bd56177673f77aa388687740d427832357a754
Leturgone <finekon221@gmail.com>
2026-01-06 18:03:19 +0300

    add actual FolderSelector for desktop

---
+++ composeApp/src/jvmMain/kotlin/compose/project/listingstowordconverter/presentation/ui/components/folderselector/FolderSelector.jvm.kt
@@ -0,0 +1,7 @@
+package compose.project.listingstowordconverter.presentation.ui.components.folderselector
+
+actual class FolderSelector {
+    actual suspend fun selectFolder(): String? {
+        TODO("Not yet implemented")
+    }
+}
