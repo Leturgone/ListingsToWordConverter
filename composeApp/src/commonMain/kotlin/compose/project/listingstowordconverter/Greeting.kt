cda965c98713aa6d32bb55e0b405312d2053803f
Leturgone <finekon221@gmail.com>
2026-01-06 16:25:56 +0300

    delete unused sample files

--- composeApp/src/commonMain/kotlin/compose/project/listingstowordconverter/Greeting.kt
+++
@@ -1,9 +0,0 @@
-package compose.project.listingstowordconverter
-
-class Greeting {
-    private val platform = getPlatform()
-
-    fun greet(): String {
-        return "Hello, ${platform.name}!"
-    }
-}
