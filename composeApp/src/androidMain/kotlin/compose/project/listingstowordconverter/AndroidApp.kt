6bf663ec62e172fa3607f96af3c3b8eedebc16b3
Leturgone <finekon221@gmail.com>
2026-01-06 16:26:56 +0300

    add AndroidApp

---
+++ composeApp/src/androidMain/kotlin/compose/project/listingstowordconverter/AndroidApp.kt
@@ -0,0 +1,13 @@
+package compose.project.listingstowordconverter
+
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.Composable
+import compose.project.listingstowordconverter.presentation.ui.screens.AndroidUploadScreen
+
+
+@Composable
+fun AndroidApp() {
+    MaterialTheme {
+        AndroidUploadScreen()
+    }
+}
