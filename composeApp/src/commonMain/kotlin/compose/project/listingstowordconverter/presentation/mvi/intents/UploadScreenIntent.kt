7d6e6620e3e8804cf3fc4f36a3ab0bf672dc55df
Leturgone <finekon221@gmail.com>
2026-01-06 20:26:59 +0300

    add intents

--- composeApp/src/commonMain/kotlin/compose/project/listingstowordconverter/presentation/mvi/intents/UploadScreenIntent.kt
+++ composeApp/src/commonMain/kotlin/compose/project/listingstowordconverter/presentation/mvi/intents/UploadScreenIntent.kt
@@ -1,6 +1,6 @@
 package compose.project.listingstowordconverter.presentation.mvi.intents
 
 sealed class UploadScreenIntent {
-    object LoadFilesByDragAndDrop: UploadScreenIntent()
+    data class LoadFilesByDragAndDrop(val path: String): UploadScreenIntent()
     object  LoadFilesByOpenExplorer: UploadScreenIntent()
 }
