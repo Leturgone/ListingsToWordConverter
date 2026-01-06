c79f4f7b4eaaf3e93fd82e89c34acf9282a30716
Leturgone <finekon221@gmail.com>
2026-01-06 18:58:09 +0300

    change fun in FolderSelector

--- composeApp/src/commonMain/kotlin/compose/project/listingstowordconverter/presentation/ui/components/folderselector/FolderSelector.kt
+++ composeApp/src/commonMain/kotlin/compose/project/listingstowordconverter/presentation/ui/components/folderselector/FolderSelector.kt
@@ -1,5 +1,9 @@
 package compose.project.listingstowordconverter.presentation.ui.components.folderselector
 
+import androidx.compose.runtime.Composable
+
 expect class FolderSelector {
-    suspend fun selectFolder(): String?
+
+    @Composable
+    fun selectFolder(): suspend () -> String?
 }
