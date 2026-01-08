c837c2fa03e759b9810716e0d915b04d7bbb090c
Leturgone <finekon221@gmail.com>
2026-01-08 21:53:32 +0300

    add contract for folder selector

---
+++ composeApp/src/androidMain/kotlin/compose/project/listingstowordconverter/presentation/ui/components/folderselector/SelectFolderContract.kt
@@ -0,0 +1,28 @@
+package compose.project.listingstowordconverter.presentation.ui.components.folderselector
+
+import android.app.Activity
+import android.content.Context
+import android.content.Intent
+import androidx.activity.result.contract.ActivityResultContract
+
+class SelectFolderContract: ActivityResultContract<Unit, String?>() {
+    override fun createIntent(
+        context: Context,
+        input: Unit
+    ): Intent {
+        return Intent(Intent.ACTION_OPEN_DOCUMENT_TREE).apply {
+            addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION)
+            addCategory(Intent.CATEGORY_DEFAULT)
+            type = "vnd.android.document/directory"
+        }
+    }
+
+    override fun parseResult(
+        resultCode: Int,
+        intent: Intent?
+    ): String? {
+        if (resultCode != Activity.RESULT_OK || intent == null) return null
+        return intent.data?.path
+    }
+
+}
