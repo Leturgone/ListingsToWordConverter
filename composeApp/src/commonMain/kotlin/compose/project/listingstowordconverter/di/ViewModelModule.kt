ea24a10a53abeb4b1d01685042793ebb5ea93bec
Leturgone <finekon221@gmail.com>
2026-01-10 02:17:04 +0300

    add ViewModel module to DI

---
+++ composeApp/src/commonMain/kotlin/compose/project/listingstowordconverter/di/ViewModelModule.kt
@@ -0,0 +1,22 @@
+package compose.project.listingstowordconverter.di
+
+import compose.project.listingsconverter.domain.repository.FileRepository
+import compose.project.listingsconverter.domain.repository.WordRepository
+import compose.project.listingstowordconverter.data.repository.FileRepositoryImpl
+import compose.project.listingstowordconverter.data.repository.WordRepositoryImpl
+import compose.project.listingstowordconverter.domain.usecase.ConvertCodeToWordFileUseCase
+import compose.project.listingstowordconverter.presentation.viewmodel.UploadScreenViewModel
+import org.koin.core.module.dsl.viewModel
+import org.koin.dsl.module
+
+val viewModelModule = module {
+    single<FileRepository> { FileRepositoryImpl(fileSystem = get()) }
+    single<WordRepository> { WordRepositoryImpl() }
+
+    single<ConvertCodeToWordFileUseCase> {
+        ConvertCodeToWordFileUseCase(fileRepository = get(), wordRepository = get())
+    }
+    viewModel { UploadScreenViewModel(get(),get()) }
+
+
+}
