3d346bca64ab1cb6de629c7ca8b896eec57d2d26
Leturgone <finekon221@gmail.com>
2026-01-10 02:19:22 +0300

    add koin setup to main activity

--- composeApp/src/androidMain/kotlin/compose/project/listingstowordconverter/MainActivity.kt
+++ composeApp/src/androidMain/kotlin/compose/project/listingstowordconverter/MainActivity.kt
@@ -9,17 +9,22 @@ import androidx.compose.ui.tooling.preview.Preview
 import compose.project.listingstowordconverter.di.androidModule
 import compose.project.listingstowordconverter.di.appFileSystemModule
 import compose.project.listingstowordconverter.di.viewModelModule
+import compose.project.listingstowordconverter.presentation.ui.components.folderselector.FolderSelector
+import org.koin.android.ext.android.getKoin
 import org.koin.android.ext.koin.androidContext
+import org.koin.compose.getKoin
 import org.koin.core.context.startKoin
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         enableEdgeToEdge()
         super.onCreate(savedInstanceState)
+
         startKoin {
             androidContext(this@MainActivity)
             modules(appFileSystemModule + viewModelModule + androidModule)
         }
+        getKoin().get<FolderSelector>().init(this)
         setContent {
             AndroidApp()
         }
