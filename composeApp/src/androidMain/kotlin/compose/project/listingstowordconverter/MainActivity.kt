d9171262af2dc890a18b7b66701067329ed620de
Leturgone <finekon221@gmail.com>
2026-01-09 20:08:46 +0300

    add startKoin to Android

--- composeApp/src/androidMain/kotlin/compose/project/listingstowordconverter/MainActivity.kt
+++ composeApp/src/androidMain/kotlin/compose/project/listingstowordconverter/MainActivity.kt
@@ -6,12 +6,20 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
+import compose.project.listingstowordconverter.di.androidModule
+import compose.project.listingstowordconverter.di.appFileSystemModule
+import compose.project.listingstowordconverter.di.viewModelModule
+import org.koin.android.ext.koin.androidContext
+import org.koin.core.context.startKoin
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         enableEdgeToEdge()
         super.onCreate(savedInstanceState)
-
+        startKoin {
+            androidContext(this@MainActivity)
+            modules(appFileSystemModule + viewModelModule + androidModule)
+        }
         setContent {
             AndroidApp()
         }
