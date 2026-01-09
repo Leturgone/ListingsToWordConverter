4fb8e3ca174d8148f30b7abc623d5a4f6a3a4c03
Leturgone <finekon221@gmail.com>
2026-01-10 02:18:59 +0300

    add viewModel to AndroidUploadScreen

--- composeApp/src/androidMain/kotlin/compose/project/listingstowordconverter/presentation/ui/screens/AndroidUploadScreen.kt
+++ composeApp/src/androidMain/kotlin/compose/project/listingstowordconverter/presentation/ui/screens/AndroidUploadScreen.kt
@@ -2,13 +2,20 @@ package compose.project.listingstowordconverter.presentation.ui.screens
 
 import androidx.compose.runtime.Composable
 import compose.project.listingstowordconverter.presentation.mvi.DataState
+import compose.project.listingstowordconverter.presentation.mvi.intents.UploadScreenIntent
 import compose.project.listingstowordconverter.presentation.ui.screens.uploadScreen.UploadCard
+import compose.project.listingstowordconverter.presentation.viewmodel.UploadScreenViewModel
+import org.koin.compose.viewmodel.koinViewModel
 
 @Composable
 fun AndroidUploadScreen(){
+
+    val viewModel = koinViewModel<UploadScreenViewModel>()
+
     UploadCard(
-        onSelectFolderClicked = {},
-        onFolderDropped = {},
+        onSelectFolderClicked = {
+            viewModel.processIntent(UploadScreenIntent.LoadFilesByOpenExplorer)
+        },
         isDragEnabled = false,
         status = DataState.Default
     )
