e4a78f9eeb139a8699040a4204e6734dfbb6e877
Leturgone <finekon221@gmail.com>
2026-01-09 20:08:37 +0300

    add startKoin to JVM

--- composeApp/src/jvmMain/kotlin/compose/project/listingstowordconverter/main.kt
+++ composeApp/src/jvmMain/kotlin/compose/project/listingstowordconverter/main.kt
@@ -2,12 +2,19 @@ package compose.project.listingstowordconverter
 
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
+import compose.project.listingstowordconverter.di.appFileSystemModule
+import compose.project.listingstowordconverter.di.viewModelModule
+import compose.project.listingstowordconverter.di.jvmModule
+import org.koin.core.context.startKoin
 
 fun main() = application {
     Window(
         onCloseRequest = ::exitApplication,
         title = "ListingsToWordConverter",
     ) {
+        startKoin {
+            modules(appFileSystemModule +viewModelModule+ jvmModule)
+        }
         DesktopApp()
     }
 }
