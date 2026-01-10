3141a814de5ad29cd52c3673aa7bd14b277ec11e
Leturgone <finekon221@gmail.com>
2026-01-11 00:17:39 +0300

    edit FolderSelector for desktop

--- composeApp/src/jvmMain/kotlin/compose/project/listingstowordconverter/presentation/ui/components/folderselector/FolderSelector.jvm.kt
+++ composeApp/src/jvmMain/kotlin/compose/project/listingstowordconverter/presentation/ui/components/folderselector/FolderSelector.jvm.kt
@@ -1,35 +1,19 @@
 package compose.project.listingstowordconverter.presentation.ui.components.folderselector
 
-import androidx.compose.runtime.Composable
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 import java.awt.FileDialog
 import java.awt.Frame
 
-actual class FolderSelector {
+actual class FolderSelector actual constructor() {
+    actual suspend fun selectFolder(): String? = withContext(Dispatchers.Main) {
+        val dialog = FileDialog(null as Frame?, "Выберите папку", FileDialog.LOAD)
+        dialog.isMultipleMode = false
+        dialog.isVisible = true
 
-    @Composable
-    actual fun selectFolder(): suspend () -> String? {
-        return suspend {
-            withContext(Dispatchers.Main){
-                val dialog = FileDialog(null as Frame?, "Выберите папку", FileDialog.LOAD)
-
-                dialog.mode = FileDialog.LOAD
-
-                // Only one folder
-                dialog.isMultipleMode = false
-
-                dialog.isVisible = true
-
-                val directory = dialog.directory
-                val file = dialog.file
-
-                when {
-                    directory != null && file != null -> "$directory$file"
-                    directory != null -> directory
-                    else -> null
-                }
-            }
+        dialog.directory?.let { directory ->
+            dialog.file?.let { file -> "$directory$file" } ?: directory
         }
     }
 }
+
