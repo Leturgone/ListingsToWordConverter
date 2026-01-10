0fd2e25b6d315af8693dba018f0fad5de6fc2854
Leturgone <finekon221@gmail.com>
2026-01-11 00:56:21 +0300

    add state to AndroidUploadScreen

--- composeApp/src/androidMain/kotlin/compose/project/listingstowordconverter/presentation/ui/screens/AndroidUploadScreen.kt
+++ composeApp/src/androidMain/kotlin/compose/project/listingstowordconverter/presentation/ui/screens/AndroidUploadScreen.kt
@@ -1,7 +1,8 @@
 package compose.project.listingstowordconverter.presentation.ui.screens
 
 import androidx.compose.runtime.Composable
-import compose.project.listingstowordconverter.presentation.mvi.DataState
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import compose.project.listingstowordconverter.presentation.mvi.intents.UploadScreenIntent
 import compose.project.listingstowordconverter.presentation.ui.screens.uploadScreen.UploadCard
 import compose.project.listingstowordconverter.presentation.viewmodel.UploadScreenViewModel
@@ -11,12 +12,13 @@ import org.koin.compose.viewmodel.koinViewModel
 fun AndroidUploadScreen(){
 
     val viewModel = koinViewModel<UploadScreenViewModel>()
+    val uiState by viewModel.state.collectAsState()
 
     UploadCard(
         onSelectFolderClicked = {
             viewModel.processIntent(UploadScreenIntent.LoadFilesByOpenExplorer)
         },
         isDragEnabled = false,
-        status = DataState.Default
+        status = uiState
     )
 }
