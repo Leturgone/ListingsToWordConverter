ba3316ab30cdfaf31e5cc9d3e89eba6e1a6c5609
Leturgone <finekon221@gmail.com>
2026-01-11 00:19:21 +0300

    delete package

--- composeApp/src/commonMain/kotlin/compose/project/listingstowordconverter/presentation/ui/components/folderselector/FolderSelector.kt
+++ composeApp/src/commonMain/kotlin/compose/project/listingstowordconverter/presentation/ui/components/folderselector/FolderSelector.kt
@@ -1,7 +1,5 @@
 package compose.project.listingstowordconverter.presentation.ui.components.folderselector
 
-import androidx.compose.runtime.Composable
-
 expect class FolderSelector() {
 
     suspend fun selectFolder(): String?
