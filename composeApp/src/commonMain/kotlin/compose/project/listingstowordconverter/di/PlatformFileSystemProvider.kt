a8d93db0b48d705bf91d3312a6dcc49df2ecc8cf
Leturgone <finekon221@gmail.com>
2026-01-11 00:11:29 +0300

    delete unused platform provider

--- composeApp/src/commonMain/kotlin/compose/project/listingstowordconverter/di/PlatformFileSystemProvider.kt
+++
@@ -1,7 +0,0 @@
-package compose.project.listingstowordconverter.di
-
-import okio.FileSystem
-
-expect class PlatformFileSystemProvider(): FileSystemProvider {
-    override fun provide(): FileSystem
-}
